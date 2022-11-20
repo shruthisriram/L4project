83a4c6d9690dcb7af1d27843e734477cfd0fc936
shruthisriram <98127024+shruthisriram@users.noreply.github.com>
2022-11-20 19:22:37 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,1 +1,25 @@
-h
+const todoList=()=>{
+    let all=[];
+    const add=(todoItem)=>{
+      all.push(todoItem);
+    };
+    const markAsComplete=(index)=>{
+      all[index].completed=true;
+    };
+   
+ const overdue=()=>{
+      return all.filter(
+        (item)=>item.dueDate<new Date().toLocaleDateString("en-CA"));
+    };
+  const dueToday=()=>{
+     return all.filter(
+        (item)=>item.dueDate===new Date().toLocaleDateString("en-CA"));
+     
+    };
+  const dueLater=()=>{
+      return all.filter(
+        (item)=>item.dueDate > new Date().toLocaleDateString("en-CA"));
+    };
+    return{all,add,markAsComplete,dueLater,overdue,dueToday};
+  };
+  module.exports=todoList;
